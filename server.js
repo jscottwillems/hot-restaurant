252c099a55dba93725f59ebfb6c6f2683499747a
notsilverman <birchba@gmail.com>
2018-07-28 13:07:58 -0500

    things

--- server.js
+++ server.js
@@ -13,3 +13,29 @@ app.use(bodyParser.json());
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
+
+let table = [
+
+]
+
+
+
+app.get("/api/tables", function(req, res) {
+    return res.json(table);
+  });
+
+  app.get("/input", function(req, res) {
+    res.sendFile(path.join(__dirname, "input.html"));
+  });
+
+
+
+  app.post("/api/tables", function(req, res) {
+    var newTable = req.body;
+
+    console.log(newTable);
+  
+    table.push(newTable);
+  
+    res.json(newTable);
+  });
